8ee64a88eaf6c8c84ba4ab3f4a393cadf855791b
Arkady Elterman <elterman@gmail.com>
2025-04-22 09:47:20 -0700

    .

--- src/Board.svelte
+++ src/Board.svelte
@@ -38,7 +38,7 @@
         <Cell {cell} />
     {/each}
     <img class="img" src={Trillium} alt="Trillium" width={csz * 0.8} style="margin-top: {(csz / 2.6) * sqrt3}px" />
-    {#each [1, 2, 3, 4, 5, 6, 7, 8, 9] as pos (pos)}
+    {#each range(12) as pos (pos)}
         <Dot {pos} />
     {/each}
 </div>
